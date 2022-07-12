c6327aa6bffd786e4285e35f94246baa0b554131
jrose53 <107949691+jrose53@users.noreply.github.com>
2022-07-12 10:07:35 +0200

    Added weather icons

--- src/js.js
+++ src/js.js
@@ -43,6 +43,7 @@ let datum = document.querySelector("#datum");
 datum.innerHTML = formatDate(now);
 
 function displayTemperature(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
@@ -57,7 +58,6 @@ function displayTemperature(response) {
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
